c5f87698aab962441c9653829aed1b2a94b425ba
sadohu <sadohu26@yahoo.com>
2023-10-14 03:11:05 -0500

    Add: ListadoController logic

--- C6-iOS-CL2-LaBuenaComida/Controller/ListadoViewController.swift
+++ C6-iOS-CL2-LaBuenaComida/Controller/ListadoViewController.swift
@@ -7,11 +7,20 @@
 
 import UIKit
 
-class ListadoViewController: UIViewController {
+class ListadoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     @IBOutlet weak var tvPlatos: UITableView!
+    var platosList : [PlatoEntity] = [];
+    var intexPlato = -1;
+    let refreshControl = UIRefreshControl();
     
     override func viewDidLoad() {
         super.viewDidLoad();
+        platosList = PlatoController().listPlatos();
+        tvPlatos.dataSource = self;
+        tvPlatos.delegate = self;
+        
+        refreshControl.addTarget(self, action: #selector(updateData), for: .valueChanged);
+        tvPlatos.refreshControl = refreshControl;
     }
     
     @IBAction func btnNuevo(_ sender: UIButton) {
@@ -20,5 +29,24 @@ class ListadoViewController: UIViewController {
 
     @IBAction func btnRefresh(_ sender: UIButton) {
         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return platosList.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let view = tvPlatos.dequeueReusableCell(withIdentifier: "itemPlato") as! ItemPlatoTableViewCell;
+        let descripcion = platosList[indexPath.row].descripcion;
+        let precio = String(platosList[indexPath.row].precio);
+        view.lblDescripcion.text = descripcion;
+        view.lblPrecio.text = "S/ " + precio;
+        return view;
+    }
+    
+    @objc func updateData() {
+        platosList = PlatoController().listPlatos();
+        self.tvPlatos.reloadData();
+        self.refreshControl.endRefreshing();
     }
 }
