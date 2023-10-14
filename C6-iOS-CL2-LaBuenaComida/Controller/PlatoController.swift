d6ce82a7b34dc4ccae45def365527787d55d4f7b
sadohu <sadohu26@yahoo.com>
2023-10-14 02:52:55 -0500

    Add: PlatosController logic (List, Add)

--- C6-iOS-CL2-LaBuenaComida/Controller/PlatoController.swift
+++ C6-iOS-CL2-LaBuenaComida/Controller/PlatoController.swift
@@ -11,10 +11,32 @@ class PlatoController: NSObject {
     func addPlato(bean : Plato){
         let delegate = UIApplication.shared.delegate as! AppDelegate;
         let context = delegate.persistentContainer.viewContext;
+        
+        let entity = PlatoEntity(context: context);
+        entity.codigo = Int16(bean.codigo);
+        entity.descripcion = bean.descripcion;
+        entity.precio = bean.precio;
+        entity.stock = Int16(bean.stock);
+        entity.chef = bean.chef;
+        
+        do{
+            try context.save();
+        } catch(let e as NSError){
+            print(e.localizedDescription);
+        }
     }
     
-    func listPlatos(){
+    func listPlatos() -> [PlatoEntity]{
         let delegate = UIApplication.shared.delegate as! AppDelegate;
         let context = delegate.persistentContainer.viewContext;
+        
+        var result : [PlatoEntity]!;
+        do{
+            let data = PlatoEntity.fetchRequest();
+            result = try context.fetch(data);
+        } catch(let e as NSError){
+            print(e.localizedDescription);
+        }
+        return result;
     }
 }
